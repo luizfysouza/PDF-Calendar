abfc533779b0995980f5d74dda00d2bd177b26be
Luisa Amaral <52139564+luisaoamaral@users.noreply.github.com>
2019-06-27 09:06:49 -0300

    Update Funcoes.c

--- Funcoes.c
+++ Funcoes.c
@@ -1,17 +1,17 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "Header.h"
 
-void FLVazia(TipoLista *Lista) {
+void FLVazia(TipoLista *Lista)
+{
     Lista->Primeiro = (Apontador) malloc(sizeof(Celula));
     Lista->Ultimo = Lista->Primeiro;
     Lista->Primeiro->Prox = NULL;
 }
 
-void Insere(TipoItem x, TipoLista *Lista) {
-    Lista->Ultimo->Prox = (Apontador) malloc(sizeof(Celula));
+void Insere(TipoItem x, TipoLista *Lista){
+    Lista->Ultimo->Prox =(Apontador) malloc(sizeof(Celula));
     Lista->Ultimo = Lista->Ultimo->Prox;
     Lista->Ultimo->Item = x;
     Lista->Ultimo->Prox = NULL;
@@ -60,10 +60,46 @@ void InsereLinear(TipoItem *A, TipoItem *F, TipoItem *V, TipoItem *O, Apontador 
                 break;
         }
         Busca2 = Busca2->Prox;
+    
     }
+}
 
-    for (int g = 0; g < i; g++) {
-        printf("%c %d %d %d \n", A[g].Evento, A[g].Dia, A[g].Mes, A[g].Ano);
-        printf("%s\n", A[g].Especificacao);
+void Ordena(TipoItem *Z, int n){
+   // n-=1;
+    int i , j, Min ;
+    TipoItem x;
+    for ( i = 0; i < n; i++){
+        Min = i ;
+        for ( j = i+1; j < n; j++)
+            if (Z[j].Dia < Z[Min].Dia)
+                Min = j ;
+        x = Z[Min] ;
+        Z[Min] = Z[i] ;
+        Z[i] = x;
     }
+    for (int g = 0; g < n; g++) {
+        printf("%c %d %d %d ", Z[g].Evento, Z[g].Dia, Z[g].Mes, Z[g].Ano);
+        printf("%s\n", Z[g].Especificacao);
+    }
+}
+
+void PesquisaEvento (TipoItem *P, int t){
+    for (int v=0; v<t; v++){
+        printf("%c %d %d %d ", P[v].Evento, P[v].Dia, P[v].Mes, P[v].Ano);
+        printf("%s\n", P[v].Especificacao);
+    }
+}
+
+int DiasDoMes (int y, int *mes, int x, int m){
+    int i;
+    if (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0))
+        mes[1] = 29;
+    for (i = 1900; i < y; i++)
+        if (i % 4 == 0 && (i % 100 != 0 || i % 400 == 0))
+            x += 366;
+        else
+            x += 365;
+    for (i = 0; i < m - 1; i++)
+        x += mes[i];
+    return x;
 }
